733e5f9a52b9561654bf50295b27ca8793b7b963
Ihor Savenko <csigorek@gmail.com>
2024-08-21 22:13:44 +0100

    add umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,19 +14,21 @@ export const metadata = {
 // Root layout component for the application
 export default function RootLayout( { children }: { children: React.ReactNode } ): JSX.Element {
   return (
-    <html lang="en">
+      <html lang="en">
       {/*
         The <head /> component will contain the components returned by the nearest parent
         head.js. It can be used to define the document head for SEO, metadata, and other purposes.
         Learn more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <script async src="https://umami-azure-one.vercel.app/script.js"
+              data-website-id="b26f43ac-2e62-4378-b63b-767ef44d6db6"></script>
+      <head/>
       <body>
-        {/* Wrap the children with the AuthContextProvider to provide authentication context */}
-        <AuthContextProvider>
+      {/* Wrap the children with the AuthContextProvider to provide authentication context */}
+      <AuthContextProvider>
           {children}
-        </AuthContextProvider>
+      </AuthContextProvider>
       </body>
-    </html>
+      </html>
   );
 }
